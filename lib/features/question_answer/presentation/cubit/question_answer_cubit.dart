0a5e0b405e3727afe44924ec5466358902586ad2
https://github.com/Cholponai01 <cholponai.amangeldikyzy.bj@gmail.com>
2024-02-06 13:14:25 +0600

    first commit

---
+++ lib/features/question_answer/presentation/cubit/question_answer_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'question_answer_state.dart';
+
+class QuestionAnswerCubit extends Cubit<QuestionAnswerState> {
+  QuestionAnswerCubit() : super(QuestionAnswerInitial());
+}
---
+++ lib/features/zikir/presentation/cubit/zikir_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'zikir_state.dart';
+
+class ZikirCubit extends Cubit<ZikirState> {
+  ZikirCubit() : super(ZikirInitial());
+}
