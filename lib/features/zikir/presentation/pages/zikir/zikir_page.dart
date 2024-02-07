5832284d8cbca536a41b9a0b7c8c9561c9c42801
Eraly <eraly.madishov@gmail.com>
2024-02-07 15:42:35 +0600

    Add images to the project

--- lib/features/zikir/presentation/pages/zikir/zikir_page.dart
+++ lib/features/zikir/presentation/pages/zikir/zikir_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 @RoutePage()
 class ZikirPage extends StatefulWidget {
@@ -12,8 +13,40 @@ class ZikirPage extends StatefulWidget {
 class _ZikirPageState extends State<ZikirPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Zikir Page')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Зикир"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset(
+                "assets/svg/zikir/burger_slider.svg",
+              ),
+              //SvgPicture.asset("asstes/svg/main/book.svg"),
+              // InkWell(
+              //   onTap: () {},
+              //   child: SvgPicture.asset("asstes/svg/zikir/burger_slider.svg"),
+              // ),
+              Container(
+                width: 200,
+                height: 200,
+                child: Column(
+                  children: [
+                    Text(" لا اله الا أنت سبحانك اني كنت من الظالمين"),
+                    Text(
+                        "Ля иляха илля Анта! Субханака, инни кунту мина-з-залимин!"),
+                    Text(
+                        "Нет Бога достойного поклонения, кроме Тебя, Пречист Ты, поистине, я был одним из несправедливых"),
+                    SvgPicture.asset("asstes/svg/zikir/color.svg"),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
